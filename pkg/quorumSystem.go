e910ff412d1e226d6426984ec3f4db6d5e845f06
Samuele Resca <samuele.resca@gmail.com>
2021-05-26 21:50:00 +0100

    Initialization methods.

--- pkg/quorumSystem.go
+++ pkg/quorumSystem.go
@@ -1,1 +1,20 @@
 package pkg
+
+
+type QuorumSystem struct {
+	Reads GenericExpr
+	Writes GenericExpr
+	XtoNode map[string]Node
+}
+
+func DefQuorumSystem(reads GenericExpr, writes GenericExpr) QuorumSystem {
+	return QuorumSystem{}
+}
+
+func DefQuorumSystemWithReads(reads GenericExpr) QuorumSystem {
+	return QuorumSystem{}
+}
+
+func DefQuorumSystemWithWrites(reads GenericExpr) QuorumSystem {
+	return QuorumSystem{}
+}
